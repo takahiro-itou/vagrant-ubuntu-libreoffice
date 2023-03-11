2e42733a15b872e14e932fcf8bc79fad08ccf677
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-03-11 21:31:57 +0900

    容量 50GB のハードディスクを追加

--- tools/resume.sh
+++ tools/resume.sh
@@ -3,7 +3,7 @@
 scriptDir=$(dirname  $0)
 vagrantDir=${scriptDir}/../vagrant
 pushd  ${vagrantDir}
-mkdir  -p  /cygdrive/w/Vagrant/ubuntu-develop/vagrant
+mkdir  -p  /cygdrive/w/Vagrant/ubuntu-libreoffice/vagrant
 
 time  vagrant  resume ; echo 0
 
