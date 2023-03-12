cd44eadf78f31b85b4e4dc447247c0bece0ca05c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-03-12 13:44:49 +0900

    念のためパーミッションを再設定

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -22,6 +22,9 @@ sudo  mkdir        /ramdisk
 sudo  chmod  1777  /ramdisk
 echo  -e  "tmpfs\t/ramdisk\ttmpfs\trw,size=2048m,x-gvfs-show\t0\t0"  \
     |  sudo  tee -a  /etc/fstab
+
 sudo  mount  -a
+sudo  chmod  1777  /ext-hdd/data
+sudo  chmod  1777  /ramdisk
 
 date  >  /root/.provision.root
