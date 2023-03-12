1d21c48900d91342bb5309274181a074daf0a352
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-03-12 13:34:19 +0900

    新しく接続したハードディスクにパーティションを作成

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -6,11 +6,15 @@ echo  Provisioning $HOSTNAME
 
 sudo  timedatectl  set-timezone Asia/Tokyo
 
+# New HDD (/dev/sdc)
+sudo  parted  --script --align optimal  /dev/sdc -- mklabel gpt
+sudo  parted  --script --align optimal  /dev/sdc -- mkpart primary ext3 1 -1
+
 # RamDisk
 sudo  mkdir        /ramdisk
 sudo  chmod  1777  /ramdisk
 echo  -e  "tmpfs\t/ramdisk\ttmpfs\trw,size=2048m,x-gvfs-show\t0\t0"  \
-      |  sudo  tee -a  /etc/fstab
+    |  sudo  tee -a  /etc/fstab
 sudo  mount  -a
 
 date  >  /root/.provision.root
