8173dc5e81cf4c708944c439001fd76da3a94a02
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-11-09 20:50:28 +0900

    仮想マシンに転送するファイルを一か所に纏めた

--- tools/99_initialize.sh
+++ tools/99_initialize.sh
@@ -3,6 +3,10 @@
 script_dir=$(dirname "$0")
 vagrant_dir="${script_dir}/../vagrant"
 
+# 所定のディレクトリを、仮想マシンへ転送する用意をする
+project_base_dir=$(readlink -f "${script_dir}/..")
+rsync -av ~/VagrantConfig/ "${project_base_dir}/data/home/vagrant/"
+
 pushd  "${vagrant_dir}"
 mkdir  -p  /cygdrive/w/Vagrant/ubuntu-libreoffice/vagrant
 
