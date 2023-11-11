a21c1d879a176b7cead65dd1af8cefbf3c9cba9d
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-11-11 18:40:09 +0900

    プロビジョニングの処理を修正

--- tools/99_initialize.sh
+++ tools/99_initialize.sh
@@ -5,8 +5,10 @@ vagrant_dir="${script_dir}/../vagrant"
 
 # 所定のディレクトリを、仮想マシンへ転送する用意をする
 project_base_dir=$(readlink -f "${script_dir}/..")
-/bin/bash -xue "${vagrant_dir}/setup-userdata.sh"
-rsync -av ~/VagrantConfig/ "${project_base_dir}/data/home/vagrant/"
+
+env WORKDIR='/cygdrive/w/Vagrant'   \
+    /bin/bash -xue "${vagrant_dir}/setup-userdata.sh"   \
+    ;
 
 pushd "${vagrant_dir}"
 mkdir -p /cygdrive/w/Vagrant/ubuntu-libreoffice/vagrant
