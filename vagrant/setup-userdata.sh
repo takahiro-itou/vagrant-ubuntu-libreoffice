c37a5da31da8f4948db62f068b8208641397b993
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-11-11 11:11:25 +0900

    リファクタリング

--- vagrant/setup-userdata.sh
+++ vagrant/setup-userdata.sh
@@ -10,6 +10,6 @@ rsync -av ~/VagrantConfig/ "${project_base_dir}/data/home/vagrant/"
 
 # 転送するディレクトリをアーカイブしておく
 pushd "${project_base_dir}"
-rm -f vagrant/data.tar.xz
-time  tar -cJvf vagrant/data.tar.xz data/
+rm -f "${vagrant_dir}/data.tar.xz"
+time  tar -cJvf "${vagrant_dir}/data.tar.xz" data/
 popd
