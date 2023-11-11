16d171afa33d3eb2dd8b0cb27143222d21a2458d
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-11-11 10:57:44 +0900

    リファクタリング

--- vagrant/setup-userdata.sh
+++ vagrant/setup-userdata.sh
@@ -3,6 +3,13 @@
 script_dir=$(dirname "$0")
 
 project_base_dir=$(readlink -f "${script_dir}/..")
+vagrant_dir="${project_base_dir}/vagrant"
 
 # 所定のディレクトリを、仮想マシンへ転送する用意をする
 rsync -av ~/VagrantConfig/ "${project_base_dir}/data/home/vagrant/"
+
+# 転送するディレクトリをアーカイブしておく
+pushd "${project_base_dir}"
+rm -f vagrant/data.tar.xz
+time  tar -cJvf vagrant/data.tar.xz data/
+popd
