e47d3abbd71513aa44f74e41511e5456325634d7
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-11-11 11:21:28 +0900

    リファクタリング

--- tools/99_initialize.sh
+++ tools/99_initialize.sh
@@ -5,13 +5,8 @@ vagrant_dir="${script_dir}/../vagrant"
 
 # 所定のディレクトリを、仮想マシンへ転送する用意をする
 project_base_dir=$(readlink -f "${script_dir}/..")
+/bin/bash -xue "${vagrant_dir}/setup-userdata.sh"
 rsync -av ~/VagrantConfig/ "${project_base_dir}/data/home/vagrant/"
-
-# 転送するディレクトリをアーカイブしておく
-pushd "${project_base_dir}"
-rm -f vagrant/data.tar.xz
-time  tar -cJvf vagrant/data.tar.xz data/
-popd
 
 pushd "${vagrant_dir}"
 mkdir -p /cygdrive/w/Vagrant/ubuntu-libreoffice/vagrant
